41c1cee171e365ae22e242bf0ce2c0a800332f1b
wanglongxun <wanglongxun@bytedance.com>
2021-05-04 11:46:59 +0800

    完成了备忘录侧边栏功能
    Change-Id: I3a5585f89a80a9a5c88657e0954361bddd6b49b1

--- app/src/main/java/com/wlx/xmood/ui/memorandum/MemorandumFragment.kt
+++ app/src/main/java/com/wlx/xmood/ui/memorandum/MemorandumFragment.kt
@@ -1,16 +1,24 @@
 package com.wlx.xmood.ui.memorandum
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
+import android.view.MenuItem
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
 import android.widget.Toast
-import androidx.appcompat.widget.Toolbar
+import android.widget.Toolbar
+import androidx.core.view.GravityCompat
+import androidx.drawerlayout.widget.DrawerLayout
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.navigation.NavigationView
 import com.wlx.xmood.R
+import com.wlx.xmood.ui.memorandum.nav.MemorandumNavAdapter
 import com.wlx.xmood.utils.TimeUtil
 
 class MemorandumFragment : Fragment() {
@@ -18,22 +26,32 @@ class MemorandumFragment : Fragment() {
     private lateinit var memorandumViewModel: MemorandumViewModel
 
     private val noteList = ArrayList<MemorandumItem>()
+    private val allnoteList = ArrayList<MemorandumItem>()
+    private val navTitleList = ArrayList<String>()
+    private lateinit var noteListAdapter: MemorandumAdapter
+    private lateinit var navListAdapter: MemorandumNavAdapter
+    private lateinit var navView: NavigationView
+    private lateinit var drawerLayout: DrawerLayout
+    private val TAG = "MemorandumFragment"
+    private lateinit var root: View
+    private var isInCatalog = false
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         init()
-        memorandumViewModel = ViewModelProvider(this).get(MemorandumViewModel::class.java)
-        val root = inflater.inflate(R.layout.fragment_memorandum, container, false)
-//        val textView: TextView = root.findViewById(R.id.text_memorandum)
-//
-//        memorandumViewModel.text.observe(viewLifecycleOwner, Observer {
-//            textView.text = it
-//        })
+        setHasOptionsMenu(true)
+        memorandumViewModel =
+            ViewModelProvider(this).get(MemorandumViewModel::class.java)
+        root = inflater.inflate(R.layout.fragment_memorandum, container, false)
+        drawerLayout = root.findViewById(R.id.memorandum_drawer_layout)
         val toolbar: Toolbar = root.findViewById(R.id.toolbar_memorandum)
         toolbar.inflateMenu(R.menu.memorandum_tool_bar)
+        toolbar.menu.findItem(R.id.add_memorandum).setShowAsAction(MenuItem.SHOW_AS_ACTION_ALWAYS);
+        toolbar.menu.findItem(R.id.search_memorandum)
+            .setShowAsAction(MenuItem.SHOW_AS_ACTION_ALWAYS);
         toolbar.setOnMenuItemClickListener {
             when (it.itemId) {
                 R.id.add_memorandum ->
@@ -43,31 +61,83 @@ class MemorandumFragment : Fragment() {
             }
             true
         }
+        val openDrawBtn: ImageView = root.findViewById(R.id.memorandum_drawer_btn)
+        openDrawBtn.setOnClickListener {
+            drawerLayout.openDrawer(GravityCompat.START)
+        }
+        navView = root.findViewById(R.id.memorandum_navView)
+        val menu = navView.menu
+        for (i in 0 until navTitleList.size) {
+            menu.add(1, i, i, navTitleList[i]).setIcon(R.drawable.ic_point_24)
+                .setOnMenuItemClickListener {
+                    changeToCatalog(navTitleList[i])
+                    true
+                }
+        }
         val recyclerView: RecyclerView = root.findViewById(R.id.memorandum_list)
         recyclerView.layoutManager = LinearLayoutManager(activity)
-        recyclerView.adapter = MemorandumAdapter(this, noteList)
+        noteListAdapter = MemorandumAdapter(this, noteList)
+        recyclerView.adapter = noteListAdapter
+        val allnote: TextView = root.findViewById(R.id.memorandum_all_note)
+        allnote.setOnClickListener {
+            changeToAll()
+        }
         return root
     }
 
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        Log.d(TAG, "onCreate: ")
+    }
+
     private fun init() {
-        noteList.add(
+        allnoteList.add(
             MemorandumItem(
                 "TCP与UDP", "TCP是有连接，UDP是无连接",
                 TimeUtil.Str2Date("2021年4月17日", "YYYY年MM月DD日"), "计网"
             )
         )
-        noteList.add(
+        allnoteList.add(
             MemorandumItem(
                 "线程与进程", "进程可以拥有多个线程，进程是操作系统内存分配的基本单位",
                 TimeUtil.Str2Date("2021年4月24日", "YYYY年MM月DD日"), "OS"
             )
         )
-        noteList.add(
+        allnoteList.add(
             MemorandumItem(
                 "线程与协程", "协程比线程更小，可以用来执行轻量级任务",
                 TimeUtil.Str2Date("2021年5月6日", "YYYY年MM月DD日"), "Android"
             )
         )
+        for (note in allnoteList) {
+            noteList.add(note)
+        }
+        navTitleList.add("计网")
+        navTitleList.add("OS")
+        navTitleList.add("Android")
+    }
+
+    private fun changeToCatalog(catalog: String) {
+        isInCatalog = true
+        noteList.clear()
+        for (note in allnoteList) {
+            if (note.catalog == catalog) {
+                noteList.add(note)
+            }
+        }
+        noteListAdapter.notifyDataSetChanged()
+        drawerLayout.closeDrawer(GravityCompat.START)
+    }
+
+    private fun changeToAll() {
+        if (!isInCatalog) {
+            return
+        }
+        noteList.clear()
+        for (note in allnoteList) {
+            noteList.add(note)
+        }
+        noteListAdapter.notifyDataSetChanged()
     }
 
 }
