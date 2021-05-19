8fb674e1a34201f1043fd8c35627c7077e1ada52
wanglongxun <wanglongxun@bytedance.com>
2021-05-19 17:10:40 +0800

    添加了账号信息页面
    Change-Id: Ia488ae2ad33f7418e6c4fadd802bcb3fee78e0bc

---
+++ app/src/main/java/com/wlx/xmood/settings/setting/userinfo/ChangePWActivity.kt
@@ -0,0 +1,12 @@
+package com.wlx.xmood.settings.setting.userinfo
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.wlx.xmood.R
+
+class ChangePWActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_change_p_w)
+    }
+}
--- app/src/main/java/com/wlx/xmood/settings/setting/userinfo/UserInfoActivity.kt
+++ app/src/main/java/com/wlx/xmood/settings/setting/userinfo/UserInfoActivity.kt
@@ -1,12 +1,28 @@
 package com.wlx.xmood.settings.setting.userinfo
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.ImageView
+import androidx.constraintlayout.widget.ConstraintLayout
+import com.wlx.xmood.ActivityCollector
 import com.wlx.xmood.BaseActivity
 import com.wlx.xmood.R
 
 class UserInfoActivity : BaseActivity() {
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_user_info)
+        val backBtn: ImageView = findViewById(R.id.user_info_back_btn)
+        backBtn.setOnClickListener {
+            finish()
+        }
+        val pwChange: ConstraintLayout = findViewById(R.id.user_info_pw_layout)
+        pwChange.setOnClickListener {
+            val intent = Intent(this, ChangePWActivity::class.java)
+            intent.putExtra("username", ActivityCollector.username)
+            startActivity(intent)
+        }
     }
 }
