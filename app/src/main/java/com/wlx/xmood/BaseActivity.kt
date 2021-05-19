943d45b6c1dec999ee013c682688e46028b424e0
wanglongxun <wanglongxun@bytedance.com>
2021-05-19 17:25:06 +0800

    拦截了返回键，两秒内连续点击返回键退出app
    Change-Id: If836ebafeace8c661065ad54cb0897d38ec10470

--- app/src/main/java/com/wlx/xmood/BaseActivity.kt
+++ app/src/main/java/com/wlx/xmood/BaseActivity.kt
@@ -1,10 +1,18 @@
 package com.wlx.xmood
 
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import androidx.appcompat.app.AppCompatActivity
+import com.wlx.xmood.utils.Utils
 
 open class BaseActivity : AppCompatActivity() {
 
+    private var isExit = false
+    private val handler = Handler(Looper.getMainLooper(), Handler.Callback {
+        isExit = false
+        true
+    })
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -16,4 +24,13 @@ open class BaseActivity : AppCompatActivity() {
         ActivityCollector.removeActivity(this)
     }
 
+    override fun onBackPressed() {
+        if (isExit) {
+            ActivityCollector.finishAll()
+        } else {
+            isExit = true
+            Utils.makeToast(applicationContext, "再次点击返回键退出程序")
+            handler.sendEmptyMessageDelayed(1, 2000)
+        }
+    }
 }
