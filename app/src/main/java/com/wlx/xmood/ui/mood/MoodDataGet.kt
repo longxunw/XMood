94305bff9d7eb81d2f6b148cfffdac8a58195bea
wanglongxun <wanglongxun@bytedance.com>
2021-06-10 12:59:38 +0800

    添加了心情根据时间段获取数据
    Change-Id: I977148c979da32364de126ead68dd1bb27b0215e

--- app/src/main/java/com/wlx/xmood/ui/mood/MoodDataGet.kt
+++ app/src/main/java/com/wlx/xmood/ui/mood/MoodDataGet.kt
@@ -3,50 +3,48 @@ package com.wlx.xmood.ui.mood
 import android.content.ContentValues
 import androidx.lifecycle.liveData
 import com.wlx.xmood.dao.MyDatabaseHelper
-import com.wlx.xmood.utils.TimeUtil
 import kotlinx.coroutines.Dispatchers
-import org.json.JSONObject
 import kotlin.coroutines.CoroutineContext
 
 object MoodDataGet {
     lateinit var dbHelper: MyDatabaseHelper
 
-    var id = 666
+//    var id = 666
 
-    var allNodeList = arrayListOf<MoodChartItem>(
-        MoodChartItem(0, TimeUtil.Str2Long("2021年6月7日 13:00","yyyy年MM月dd日 HH:mm"),
-            1,"Android真的好难好难好难好难好难好难好难", "Joyful,Angry"),
-        MoodChartItem(1, TimeUtil.Str2Long("2021年6月8日 13:09","yyyy年MM月dd日 HH:mm"),
-            3,"1111", "Joyful,Angry"),
-        MoodChartItem(2, TimeUtil.Str2Long("2021年6月8日 13:09","yyyy年MM月dd日 HH:mm"),
-            3,"1111", "Joyful,Angry"),
-        MoodChartItem(3, TimeUtil.Str2Long("2021年6月9日 13:00","yyyy年MM月dd日 HH:mm"),
-            4,"1111", "Joyful,Angry"),
-        MoodChartItem(4, TimeUtil.Str2Long("2021年6月9日 13:00","yyyy年MM月dd日 HH:mm"),
-            2,"1111", "Joyful,Angry"),
-
-        MoodChartItem(5, TimeUtil.Str2Long("2021年6月10日 4:00","yyyy年MM月dd日 HH:mm"),
-            1,"Android真的好难好难好难好难好难好难好难", "Joyful,Angry"),
-        MoodChartItem(6, TimeUtil.Str2Long("2021年6月10日 5:00","yyyy年MM月dd日 HH:mm"),
-            3,"Android真的好难好难好难好难好难好难好难", "Joyful,Angry"),
-        MoodChartItem(7, TimeUtil.Str2Long("2021年6月10日 6:00","yyyy年MM月dd日 HH:mm"),
-            2,"Android真的好难好难好难好难好难好难好难", "Joyful,Angry"),
-        MoodChartItem(8, TimeUtil.Str2Long("2021年6月10日 7:00","yyyy年MM月dd日 HH:mm"),
-            4,"Android真的好难好难好难好难好难好难好难", "Joyful,Angry"),
-        MoodChartItem(9, TimeUtil.Str2Long("2021年6月10日 12:10","yyyy年MM月dd日 HH:mm"),
-            5,"Android真的好难好难好难好难好难好难好难", "Joyful,Angry"),
-        MoodChartItem(10, TimeUtil.Str2Long("2021年6月10日 12:23","yyyy年MM月dd日 HH:mm"),
-            6,"Android真的好难好难好难好难好难好难好难", "Joyful,Angry"),
-        MoodChartItem(11, TimeUtil.Str2Long("2021年6月10日 12:31","yyyy年MM月dd日 HH:mm"),
-            4,"Android真的好难好难好难好难好难好难好难", "Joyful,Angry")
-
-    )
+//    var allNodeList = arrayListOf<MoodChartItem>(
+//        MoodChartItem(0, TimeUtil.Str2Long("2021年6月7日 13:00","yyyy年MM月dd日 HH:mm"),
+//            1,"Android真的好难好难好难好难好难好难好难", "Joyful,Angry"),
+//        MoodChartItem(1, TimeUtil.Str2Long("2021年6月8日 13:09","yyyy年MM月dd日 HH:mm"),
+//            3,"1111", "Joyful,Angry"),
+//        MoodChartItem(2, TimeUtil.Str2Long("2021年6月8日 13:09","yyyy年MM月dd日 HH:mm"),
+//            3,"1111", "Joyful,Angry"),
+//        MoodChartItem(3, TimeUtil.Str2Long("2021年6月9日 13:00","yyyy年MM月dd日 HH:mm"),
+//            4,"1111", "Joyful,Angry"),
+//        MoodChartItem(4, TimeUtil.Str2Long("2021年6月9日 13:00","yyyy年MM月dd日 HH:mm"),
+//            2,"1111", "Joyful,Angry"),
+//
+//        MoodChartItem(5, TimeUtil.Str2Long("2021年6月10日 4:00","yyyy年MM月dd日 HH:mm"),
+//            1,"Android真的好难好难好难好难好难好难好难", "Joyful,Angry"),
+//        MoodChartItem(6, TimeUtil.Str2Long("2021年6月10日 5:00","yyyy年MM月dd日 HH:mm"),
+//            3,"Android真的好难好难好难好难好难好难好难", "Joyful,Angry"),
+//        MoodChartItem(7, TimeUtil.Str2Long("2021年6月10日 6:00","yyyy年MM月dd日 HH:mm"),
+//            2,"Android真的好难好难好难好难好难好难好难", "Joyful,Angry"),
+//        MoodChartItem(8, TimeUtil.Str2Long("2021年6月10日 7:00","yyyy年MM月dd日 HH:mm"),
+//            4,"Android真的好难好难好难好难好难好难好难", "Joyful,Angry"),
+//        MoodChartItem(9, TimeUtil.Str2Long("2021年6月10日 12:10","yyyy年MM月dd日 HH:mm"),
+//            5,"Android真的好难好难好难好难好难好难好难", "Joyful,Angry"),
+//        MoodChartItem(10, TimeUtil.Str2Long("2021年6月10日 12:23","yyyy年MM月dd日 HH:mm"),
+//            6,"Android真的好难好难好难好难好难好难好难", "Joyful,Angry"),
+//        MoodChartItem(11, TimeUtil.Str2Long("2021年6月10日 12:31","yyyy年MM月dd日 HH:mm"),
+//            4,"Android真的好难好难好难好难好难好难好难", "Joyful,Angry")
+//
+//    )
 
     val nodeList = ArrayList<MoodChartItem>()
 
 
-    fun addNode(moodChartItem: MoodChartItem){
-        allNodeList.add(moodChartItem)
+    fun addNode(moodChartItem: MoodChartItem) {
+//        allNodeList.add(moodChartItem)
         val db = dbHelper.writableDatabase
         val value = ContentValues().apply {
             put("date", moodChartItem.date)
@@ -58,58 +56,80 @@ object MoodDataGet {
     }
 
     fun getAllChartNodes(query: Int) = fire(Dispatchers.IO){
-        Result.success(allNodeList)
+//        Result.success(allNodeList)
         // Just for test --- with mock data
 
-//        nodeList.clear()
-//        val db = dbHelper.writableDatabase
-//        val sql = "select * from Mood"
-//        val cursor = db.rawQuery(sql, null)
-//        cursor.apply {
-//            if(moveToFirst()) {
-//                do{
-//                    val item = MoodChartItem(
-//                        getInt(getColumnIndex("id")),
-//                        getLong(getColumnIndex("date")),
-//                        getInt(getColumnIndex("rating")),
-//                        getString(getColumnIndex("category")),
-//                        getString(getColumnIndex("event"))
-//                    )
-//                    nodeList.add(item)
-//                }while (moveToNext())
-//            }
-//        }
-//        Result.success(nodeList)
+        nodeList.clear()
+        val db = dbHelper.writableDatabase
+        val sql = "select * from Mood"
+        val cursor = db.rawQuery(sql, null)
+        cursor.apply {
+            if (moveToFirst()) {
+                do {
+                    val item = MoodChartItem(
+                        getInt(getColumnIndex("id")),
+                        getLong(getColumnIndex("date")),
+                        getInt(getColumnIndex("rating")),
+                        getString(getColumnIndex("category")),
+                        getString(getColumnIndex("event"))
+                    )
+                    nodeList.add(item)
+                } while (moveToNext())
+            }
+        }
+        Result.success(nodeList)
     }
 
     fun getChartNodeById(query: Int) : MoodChartItem? {
-//        val db = dbHelper.writableDatabase
-//        val sql = "select * from Mood where id = $query"
-//        val cursor = db.rawQuery(sql, null)
-//        var moodChartItem: MoodChartItem? = null
-//        cursor.apply {
-//            if(moveToFirst()) {
-//                moodChartItem = MoodChartItem(
-//                    getInt(getColumnIndex("id")),
-//                    getLong(getColumnIndex("date")),
-//                    getInt(getColumnIndex("rating")),
-//                    getString(getColumnIndex("category")),
-//                    getString(getColumnIndex("event"))
-//                )
-//            }
-//            close()
-//        }
-//        return moodChartItem!!
+        val db = dbHelper.writableDatabase
+        val sql = "select * from Mood where id = $query"
+        val cursor = db.rawQuery(sql, null)
+        var moodChartItem: MoodChartItem? = null
+        cursor.apply {
+            if (moveToFirst()) {
+                moodChartItem = MoodChartItem(
+                    getInt(getColumnIndex("id")),
+                    getLong(getColumnIndex("date")),
+                    getInt(getColumnIndex("rating")),
+                    getString(getColumnIndex("category")),
+                    getString(getColumnIndex("event"))
+                )
+            }
+            close()
+        }
+        return moodChartItem!!
 
         //Just for test --- with mock data
-        for (node in allNodeList) {
-            if (query == node.id) {
-                return node
-            }
-        }
-        return null
+//        for (node in allNodeList) {
+//            if (query == node.id) {
+//                return node
+//            }
+//        }
+//        return null
     }
 
+    private fun getTimeTypeCharNode(startTime: Long, endTime: Long) = fire(Dispatchers.IO) {
+        val result = ArrayList<MoodChartItem>()
+        val db = dbHelper.writableDatabase
+        var sql = "select * from Mood where date >= $startTime and date < $endTime"
+        val cursor = db.rawQuery(sql, null, null)
+        cursor.apply {
+            if (moveToFirst()) {
+                do {
+                    val item = MoodChartItem(
+                        getInt(getColumnIndex("id")),
+                        getLong(getColumnIndex("date")),
+                        getInt(getColumnIndex("rating")),
+                        getString(getColumnIndex("category")),
+                        getString(getColumnIndex("event"))
+                    )
+                    result.add(item)
+                } while (moveToNext())
+            }
+            close()
+        }
+        Result.success(result)
+    }
 
     private fun <T> fire(context: CoroutineContext, block: suspend () -> Result<T>) =
         liveData<Result<T>>(context) {
